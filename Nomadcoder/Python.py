c19f8c6f8987d645d29d722f81ebb2f4f383b1d4
bong9C <140769729+bong9C@users.noreply.github.com>
2023-08-16 11:10:39 +0000

    2023.08.16 function

---
+++ Nomadcoder/Python.py
@@ -0,0 +1,9 @@
+def say_hello():
+  print("hello how r u?")
+
+
+say_hello()
+say_hello()
+say_hello()
+say_hello()
+say_hello()
---
+++ main.py
@@ -0,0 +1,5 @@
+def say_hello():
+  print("hello how r u?")
+
+
+say_hello()
