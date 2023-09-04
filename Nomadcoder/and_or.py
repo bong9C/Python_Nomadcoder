454869997d853336297bf3bba9ee2102001c5db4
bong9C <140769729+bong9C@users.noreply.github.com>
2023-09-04 13:26:00 +0000

    2023.09.04 Control flow

--- Nomadcoder/and_or.py
+++ Nomadcoder/and_or.py
@@ -2,11 +2,11 @@ age = int(input("How old are you?"))
 
 if age < 18:
   print("You can't drink.")
-elif age >= 18 and age <= 35:    # and는 앞의 조건이 True면 반드시 뒤에 조건도 같아야 함, False 면 뒤에 조건도 같아야함
+elif age >= 18 and age <= 35:  # and는 앞의 조건이 True면 반드시 뒤에 조건도 같아야 함, False 면 뒤에 조건도 같아야함
   print("You dirnk beer!")
 elif age == 60 or age == 70:  # or 는 둘 중에 하나만 True 여도 됨
   print("Birthday party!")
-else: 
+else:
   print("Go ahead!")
 
 True and True == True
