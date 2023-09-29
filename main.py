52b5d7429c7454be345b115138099199ff7b33dd
bong9C <140769729+bong9C@users.noreply.github.com>
2023-09-29 15:20:04 +0000

    import, while

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from random import randint
 print("Welcome to Python Casino")
 pc_choice = randint(1, 50)
 
-
 playing = True
-
 
 while playing:
   user_choice = int(input("Choose number:"))
@@ -13,8 +11,9 @@ while playing:
     print("You won!")
     playing = False
   elif user_choice > pc_choice:
-   print("Lower!")
+    print("Lower!")
   elif user_choice < pc_choice:
     print("Higher!")
+
 # 멈춤을 주지 않으면 무한 반복
 # dictance 가 20보다 작으면 True로 반복.
