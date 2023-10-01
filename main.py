e1b92514a40de90b681a55225f766da021fce4bb
bong9C <140769729+bong9C@users.noreply.github.com>
2023-10-01 06:58:11 +0000

    2023.10.01 Test

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-from random import randint
-
-print("Welcome to Python Casino")
-pc_choice = randint(1, 50)
-
-playing = True
-
-while playing:
-  user_choice = int(input("Choose number:"))
-  if user_choice == pc_choice:
-    print("You won!")
-    playing = False
-  elif user_choice > pc_choice:
-    print("Lower!")
-  elif user_choice < pc_choice:
-    print("Higher!")
-
-# 멈춤을 주지 않으면 무한 반복
-# dictance 가 20보다 작으면 True로 반복.
-# 임포트는 파이썬의 공구주머니에서 공구를 가지고 와서 사용하는 것.
+a = 34
+b = 45 
+print(a+b) 
