fec2161da7a5e9f9b1ad665102d123ac8eb6bae3
bong9C <140769729+bong9C@users.noreply.github.com>
2023-10-03 14:24:03 +0000

    2023.10.03 method

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-a = 34
-b = 45 
-print(a+b) 
+days_of_week = ["Mon", "Tue", "Wed", "Thur", "Fri"]
+print(days_of_week)
+
+name = "sehee"
+print(name.upper()) # . 은 앞에 있는 것을... , upper()은 소문자를 대문자로 변환
+
+name = "sehee"
+print(name.capitalize())  # capitalize()은 첫문자를 대문자로 변환
+
+name = "sehee"
+print(name.startswith("s")) # startswith()는 첫문자가 ()안에 문자가 맞는지 확인
+
+name = "sehee"
+print(name.replace("e", "😁")) # replace("", "")는 앞에 "" 를 뒤에 ""로 변경해줌
+
+name = "sehee"
+print(name.endswith("r")) # endswith()는 마지막에 끝나는 문자가 ()안에 문자가 맞는지 확인
+# 메소드는 데이터에 결합된 펑션이다. 
+
+
